1d9fe9c1efdd4baac92e6b40f0dcbe5923458792
samuelwilk <100302646+samuelwilk@users.noreply.github.com>
2024-06-11 15:48:46 -0600

    chore: updating workflow versions

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -2,16 +2,16 @@ terraform {
 
     backend "s3" {}
 
-    required_version = ">= 1.0.0"
+    required_version = ">= 1.2.0"
 
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = ">= 3.19"
+            version = "~> 5.0"
         }
         docker = {
             source  = "kreuzwerker/docker"
-            version = ">= 2.12"
+            version = "~> 3.0"
         }
     }
 }
