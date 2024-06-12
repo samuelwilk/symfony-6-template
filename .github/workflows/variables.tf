78dc47deaecb888e16fb056a26e4f5c8078b4bf3
MaximilianGewers <maximilian.gewers@gmail.com>
2024-06-12 15:38:11 -0600

    chore: try

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,54 @@
+variable "name" {
+    type        = string
+    description = "name for the resources"
+}
+
+variable "environment" {
+    type        = string
+    description = "environment for the resources"
+}
+
+variable "image_tag" {
+    type        = string
+    description = "container image tag"
+}
+
+variable "region" {
+    type        = string
+    description = "AWS region"
+    default     = "us-west-2"
+}
+
+variable "db_tag" {
+    type        = string
+    description = "database image tag"
+    default     = "latest"
+}
+
+variable "db_name" {
+    type        = string
+    description = "database name"
+}
+
+variable "db_user" {
+    type        = string
+    description = "database user"
+}
+
+variable "db_password" {
+    type        = string
+    description = "database password"
+    sensitive   = true
+}
+
+variable "db_root_password" {
+    type        = string
+    description = "database root password"
+    sensitive   = true
+}
+
+variable "db_port" {
+    type        = number
+    description = "database internal port"
+    default     = 3306
+}
