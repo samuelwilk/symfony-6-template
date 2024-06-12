e4a712c1893e6b57f220b04b0296cf08f82a9003
MaximilianGewers <maximilian.gewers@gmail.com>
2024-06-12 15:39:15 -0600

    chore: try

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -15,20 +15,6 @@ terraform {
     }
 }
 
-variable "name" {
-    type        = string
-    description = "name for the resources"
-}
-
-variable "environment" {
-    type        = string
-    description = "environment for the resources"
-}
-
-variable "image_tag" {
-    type        = string
-    description = "container image tag"
-}
 
 locals {
     ns = "${var.name}-${var.environment}"
